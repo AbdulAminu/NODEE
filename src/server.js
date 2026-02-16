565f07df797fdacf40971233eebf513f3a1e7b06
AbdulAminu <abdulrasheedkola2002@gmail.com>
2026-02-16 14:13:54 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -20,6 +20,11 @@ await connectToDb()
 
 app.use(userRoute)
 
+
+if(process.env.NODE_ENV !== "production"){
 app.listen(3000, ()=>{
     console.log(`Server is running on http://localhost:3000`)
 })
+}
+
+export default app
